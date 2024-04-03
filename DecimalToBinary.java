3f70833dcf8815d9814e78be2f5c05a58abaf72f
Sourav Tile <147249792+SouravTile@users.noreply.github.com>
2024-04-03 11:17:35 +0530

    Update DecimalToBinary.java

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,7 +1,6 @@
 public class DecimalToBinary {
     public static void main(String[] args) 
     {
-        
         int decimal = 10; // replace with your decimal number
         String binary = "";
 
